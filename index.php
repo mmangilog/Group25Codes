8c78055df1a8ad9cb936f8d732cac4440f755b18
mmangilog <95962269+mmangilog@users.noreply.github.com>
2022-02-07 13:31:44 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,122 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewwport"content="width=device-width">
+    <title>Group 25 in TLE</title>
+    
+    <style>
+        
+    body{
+  font: 15px/2 Arial, Helvetica, sans-serif;
+  padding:0;
+  margin:0;
+  background-color:#4a2f34;
+}
+
+/* Global */
+.container{
+  width:80%;
+  margin:auto;
+  overflow:hidden;
+}
+
+/* Header **/
+header{
+  background:#6b4a50;
+  color:#d8b370;
+  padding-top:30px;
+  min-height:70px;
+  border-bottom:#351608 10px solid;
+}
+
+header a{
+  color:#d8b370;
+  text-decoration:none;
+  text-transform: uppercase;
+  font-size:16px;
+}
+
+header ul{
+  margin:0;
+  padding:0;
+}
+
+header li{
+  float-left;
+  display:inline;
+  padding: 0 20px 0 20px;
+}
+
+header #branding{
+  float:left;
+}
+
+header #branding h1{
+  margin:0;
+}
+
+header nav{
+  float:right;
+  margin-top:10px;
+}
+
+header a:hover{
+  color:#d8b370;
+  font-weight:bold;
+}
+
+footer {
+  margin: 0 auto;
+  text-align: center;
+  width: 10%;
+}
+
+    </style>
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1>Scaffold #1 in TLE</h1>
+      </div>
+        <nav>
+          <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="Teachers Involved.html">Teachers Involved</a></li>
+              <li><a href="About Us.html">About Us</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <section id="boxes">
+      <div class="container">
+        <div class="box">
+          <font color="#d8b370">
+          <h3>CAE</h3>
+          <p>Performance Task in CAE</p>
+        </font>
+      </div>
+      <div class="box">
+        <font color="#d8b370">
+        <h3>CLE</h3>
+        <p>Performance Task in CLE</p>
+      </font>
+      </div>
+        <div class="box">
+          <font color="#d8b370">
+          <h3>Mathematics</h3>
+          <p>Performance Task in Math</p>
+        </font>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <font color="#d8b370">
+      <p>Group 25 Copyright &copy; 2022</p>
+    </font>
+    </footer>
+  </body>  
+    </html>
